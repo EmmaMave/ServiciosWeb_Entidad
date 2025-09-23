bc2324806316fc8f6004d528495af439e1a09830
Zury22 <ortegazury659@gmail.com>
2025-09-23 02:43:19 +0000

    Update CarRequest.java

--- src/main/java/com/example/automovil/dto/CarRequest.java
+++ src/main/java/com/example/automovil/dto/CarRequest.java
@@ -1,8 +1,22 @@
 package com.example.automovil.dto;
 
-public record CarRequest (
-    String model,
-    String brand,
-    String color
-    //int anio
-){}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record CarRequest(
+
+    @NotBlank(message = "La marca es obligatoria")
+    String marca,
+
+    @NotBlank(message = "El modelo es obligatorio")
+    String modelo,
+
+    @NotBlank(message = "El color es obligatorio")
+    String color,
+
+   // @NotNull(message = "El año es obligatorio")
+    @Positive(message = "El año debe ser un número positivo")
+    Integer anio,
+) {}
